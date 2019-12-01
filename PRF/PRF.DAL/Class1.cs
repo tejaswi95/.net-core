685e338a09ef2d187f2b21c9898b9e7258687c4d
tejaswi95 <58379374+tejaswi95@users.noreply.github.com>
2019-11-30 23:00:30 -0800

    Add files via upload

---
+++ PRF/PRF.DAL/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace PRF.DAL
+{
+
+    public interface IWeatherData
+    {
+        void GetData();
+    }
+    public class WeatherData : IWeatherData
+    {
+        private readonly IConfiguration config;
+        public WeatherData(IConfiguration configuration)
+        {
+            config = configuration;
+        }
+        public void GetData()
+        {
+            var data  = config.GetSection("MySettings").GetSection("DbConnection").Value;
+        }
+    }
+}
---
+++ PRF/PRF/Controllers/WeatherForecastController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using PRF.DAL;
+
+namespace PRF.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IConfiguration config;
+        private readonly IWeatherData wd;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger,
+                                         IConfiguration configuration,
+                                         IWeatherData weatherData)
+        {
+            _logger = logger;
+            config = configuration;
+            wd = weatherData;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            wd.GetData();
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
+
